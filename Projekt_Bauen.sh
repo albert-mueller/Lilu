11e7190d770757b004884d659e7953d059861904
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-07 16:54:42 +0300

    Remove WhateverGreen build steps from script
    Removed the build process for WhateverGreen from the script.

--- Projekt_Bauen.sh
+++ Projekt_Bauen.sh
@@ -18,7 +18,6 @@ clone_or_update() {
 # 1. Repositories klonen oder aktualisieren
 clone_or_update "Lilu" "https://github.com/albert-mueller/Lilu/"
 clone_or_update "MacKernelSDK" "https://github.com/acidanthera/MacKernelSDK"
-clone_or_update "WhateverGreen" "https://github.com/albert-mueller/WhateverGreen/"
 
 # HINWEIS: Falls ein Fehler wegen 'xcodebuild' kommt, führe diesen Befehl 
 # EINMALIG manuell im Terminal aus (nur nötig, wenn das System es noch nicht weiß):
@@ -29,18 +28,6 @@ echo "Baue Lilu..."
 cd Lilu
 # Symlink für MacKernelSDK erstellen, falls noch nicht vorhanden
 [ ! -L "MacKernelSDK" ] && ln -s ../MacKernelSDK MacKernelSDK
-xcodebuild -configuration Debug
-cd ..
-
-# 3. WhateverGreen bauen
-echo "Baue WhateverGreen..."
-cd WhateverGreen
-# Symlink für MacKernelSDK erstellen
-[ ! -L "MacKernelSDK" ] && ln -s ../MacKernelSDK MacKernelSDK
-# Alten Symlink für Lilu entfernen, falls vorhanden
-[ -L "Lilu.kext" ] && rm -f Lilu.kext
-# Neuen Symlink setzen
-ln -s ../Lilu/build/Debug/Lilu.kext Lilu.kext
 xcodebuild -configuration Debug
 cd ..
 
